e5dc0f2524da86807db3e41e3ac9e2c82aecaace
MrUbuntu96 <bezelfree@gmail.com>
2017-09-27 13:12:42 +0300

    Improved comments

--- json/parse_json.py
+++ json/parse_json.py
@@ -2,10 +2,10 @@
 # -*- coding: utf-8 -*-
 
 import json, sys, os
-from collections import OrderedDict, defaultdict, namedtuple
+from collections import OrderedDict, defaultdict
 
 '''
-JSON decoding
+JSON unmarshalling rules
 
     JSON	    Python
     -------------------
@@ -19,49 +19,61 @@ JSON decoding
     null	=>  None
 '''
 
-Key_Value_Pair = namedtuple('Pair', ['ref', 'i'])
-
 class JSON_Processor:
     '''
-        file ==> input_dict (Ordered)
-        input_dict ==> flat_tree (Ordered)
-        input_dict ==> data_array
+        1) Load JSON file into a dict
+            >>> file ==> input_dict (Ordered)
+        2) Store JSON file as a flat tree, used to reconstruct the original file after manipulation
+            >>> input_dict ==> flat_tree (Ordered)
+        3) Store all data as a dict of key/[list of values]
+            Key include the path from root
+            All values for the same keys are stored in a list
+            >>> input_dict ==> data_map
     '''
     def __init__(self):
         self.top_object = ''
         self.input_dict = OrderedDict
         self.flat_tree  = OrderedDict()
-        self.data_array = defaultdict(list)
+        self.data_map = defaultdict(list)
 
     def reset(self, filename):
         self.__init__()
-        self.top_object = os.path.basename(filename).split('.')[0] # filename w/o extension
+        self.top_object_name = os.path.basename(filename).split('.')[0] # filename w/o extension
         self.input_dict = json.loads(open(filename).read(), object_pairs_hook = OrderedDict)
 
+    class Data_Instance:
+        '''
+            This class contains a value and a list of indices
+        '''
+        def __init__(self, ref, i_arr):
+            ''' A reference to the data value '''
+            self.ref_to_val = ref
+            ''' An array of indices that point to the data value '''
+            self.i_arr = i_arr
 
     def parse_tree_to_arrays(self, obj, obj_path=[], i_arr=[]):
-
         def add_key_value_pair(path, value, i_arr):
-
-            def path_with_index(s):
-                return ("/%s" % s) if type(s) in [str, unicode] else ("[%d]" % s)
-            def path_no_index(s):
-                return ("/%s" % s) if type(s) in [str, unicode] else ""
+            def data_path(s):
+                ''' Specific for each key/value pair, contains indices of instance within list '''
+                return "".join((("/%s" % s) if type(s) in [str, unicode] else ("[%d]" % s)) for s in path)
+            def property_path(s):
+                ''' Property path starting from root, can point to multiple values with the same key '''
+                return "".join((("/%s" % s) if type(s) in [str, unicode] else "") for s in path)
 
             ''' User reference to value (instead of value itself) '''
-            vref = [value]
-            key = "".join(path_with_index(x) for x in path)
-            self.flat_tree[key] = vref
-            vtype = "".join(path_no_index(x) for x in path)
-            self.data_array[vtype].append(Key_Value_Pair(ref=vref, i=i_arr))
-
+            ref_to_val = [value]
+            data_instance = JSON_Processor.Data_Instance(ref_to_val, i_arr)
+            self.flat_tree[data_path(path)] = ref_to_val
+            self.data_map[property_path(path)].append(data_instance)
+        # --
         if type(obj) == list:
             for i, elem in enumerate(obj):
                 self.parse_tree_to_arrays(elem, obj_path + [i], i_arr + [i])
-        elif type(obj) in [OrderedDict]:
+        elif type(obj) == OrderedDict:
             for key, val in obj.items():
                 self.parse_tree_to_arrays(val, obj_path + [key], i_arr)
         else:
+            ''' A key-value pair '''
             add_key_value_pair(obj_path, obj, i_arr)
 
     def run(self):
@@ -69,39 +81,47 @@ class JSON_Processor:
             Parse JSON file into a Dictionary
             Use DFS algorithm
             Create flat_tree for JSON reconstruction
-            Create data_array for statistical data manipulation
+            Create data_map for data manipulation: statistical analysis, security check
         '''
+        self.parse_tree_to_arrays(self.input_dict, []) #[self.top_object_name])
 
-        ''' START HERE '''
-        ''' Parse the JSON tree and generate flat_tree, data_tree '''
-        self.parse_tree_to_arrays(self.input_dict, []) #[self.top_object])
+    def print_output(self):
+        def print_title(s):
+            print s
+            print '=' * len(s)
 
-        for k, v in self.data_array.items():
-            print "%s: %d items" % (k, len(v))
-        print '--\n' * 4
+        print_title('JSON file as a flattened tree')
         for k, v in self.flat_tree.items():
             print "%s : %s" % (k, v[0])
         print '--\n' * 4
-        for k, v in self.data_array.items():
+        #
+        print_title('Map of all properties in JSON file')
+        for k, v in self.data_map.items():
+            print "%s: %d items" % (k, len(v))
+        print '--\n' * 4
+        #
+        print_title('Map of all values in JSON file')
+        for k, v in self.data_map.items():
             print '%s (%d items): ' % (str(k), len(v)),
-            for i in xrange(min(10, len(v))):
-                print '%s: %s,' % (str(v[i].ref[0]), str(v[i].i)),
-#                print str(v[i]) + ',',
-            print '....\n' if len(v)>10 else '' + '\n--'
+            for i in xrange(min(3, len(v))):
+                print '%s: %s,' % (str(v[i].ref_to_val[0]), str(v[i].i_arr)),
+            print '....' if len(v)>3 else ''
 
 #------------
 # main
 #------------
 
 def main():
+
     reload(sys)
     sys.setdefaultencoding('utf-8')
 
     if len(sys.argv) >= 2:
         try:
-            proc = JSON_Processor()
-            proc.reset(sys.argv[1])
-            proc.run()
+            processor = JSON_Processor()
+            processor.reset(sys.argv[1])
+            processor.run()
+            processor.print_output()
         except ValueError as e:
             print e
             print 'Program exited'
